f4819dfc80ecf98e6d75c6af945cffef9f24c74f
Moises Poma <gianfranco.mpc@gmail.com>
2026-06-18 15:40:50 -0500

    Primer commit

---
+++ src/main/java/com/example/tareas/TareasApplication.java
@@ -0,0 +1,21 @@
+package com.example.tareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+
+@SpringBootApplication
+public class TareasApplication {
+
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareasApplication.class, args);
+
+		System.out.println("hola");
+	}
+}
