f0d859c85e0121a3461cc7feaea0cebcda30cdaa
Moises Poma <gianfranco.mpc@gmail.com>
2026-06-18 16:26:40 -0500

    Create a simple transacton in TareasApplication

--- src/main/java/com/example/tareas/TareasApplication.java
+++ src/main/java/com/example/tareas/TareasApplication.java
@@ -1,7 +1,11 @@
 package com.example.tareas;
 
+import com.example.tareas.model.Account;
+import com.example.tareas.repository.AccountRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,6 +20,20 @@ public class TareasApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(TareasApplication.class, args);
 
-		System.out.println("hola");
+		//System.out.println("hola");
+	}
+	@Bean
+	public CommandLineRunner initData(AccountRepository accountRepository) {
+		return args -> {
+			// Validamos si ya existe para no duplicar en reinicios calientes
+			if (accountRepository.count() == 0) {
+				Account defaultAccount = new Account();
+				defaultAccount.setAccountNumber("191-445588-01");
+				defaultAccount.setBalance(2500.0);
+
+				accountRepository.save(defaultAccount);
+				System.out.println("Cajero automático inicializado: Cuenta de prueba creada con $2500.0");
+			}
+		};
 	}
 }
